4d6d0f3c3c5423e0fa9f5c8ca77781626bee5d27
Maciej Szostak <m.szostak@esoleo.pl>
2024-06-28 10:54:08 +0200

    zakres 1 javascript

--- assets/js/custom/custom.js
+++ assets/js/custom/custom.js
@@ -168,3 +168,153 @@ $(document).ready(function() {
             }
         });
     });
+
+//    Step2
+document.addEventListener("DOMContentLoaded", function() {
+    // Define form element
+    const form = document.getElementById("kt_docs_repeater_form");
+
+    if (form) {
+        // Check if Dropzone is already attached
+        if (!Dropzone.instances.length) {
+            const myDropzone = new Dropzone("#my-dropzone-element", {
+                paramName: "file",
+                maxFilesize: 2, // MB
+                acceptedFiles: ".jpeg,.jpg,.png,.gif"
+            });
+        }
+
+        // Init form validation rules. For more info check the FormValidation plugin's official documentation:https://formvalidation.io/
+        var validator = FormValidation.formValidation(
+            form,
+            {
+                plugins: {
+                    trigger: new FormValidation.plugins.Trigger(),
+                    bootstrap: new FormValidation.plugins.Bootstrap5({
+                        rowSelector: ".fv-row",
+                        eleInvalidClass: "",
+                        eleValidClass: ""
+                    }),
+                    excluded: new FormValidation.plugins.Excluded({
+                        excluded: function (field, ele, eles) {
+                            if (form.querySelector('[name="' + field + '"]') === null) {
+                                return true;
+                            }
+                        },
+                    }),
+                }
+            }
+        );
+
+        const addFields = function(index) {
+            const namePrefix = "data[" + index + "]";
+
+            // Add validators
+            validator.addField(namePrefix + "[select2_input]", {
+                validators: {
+                    notEmpty: {
+                        message: "Select2 input is required"
+                    }
+                }
+            });
+
+            validator.addField(namePrefix + "[email]", {
+                validators: {
+                    emailAddress: {
+                        message: "The value is not a valid email address"
+                    },
+                    notEmpty: {
+                        message: "Email address is required"
+                    }
+                }
+            });
+
+            validator.addField(namePrefix + "[primary][]", {
+                validators: {
+                    notEmpty: {
+                        message: "Required"
+                    }
+                }
+            });
+
+            // Initialize Select2
+            $('[name="' + namePrefix + '[select2_input]"]').select2({
+                placeholder: 'Select an option',
+                width: '100%'
+            }).on('change', function () {
+                validator.revalidateField(namePrefix + '[select2_input]');
+            });
+        };
+
+        const removeFields = function(index) {
+            const namePrefix = "data[" + index + "]";
+
+            validator.removeField(namePrefix + "[select2_input]");
+            validator.removeField(namePrefix + "[email]");
+            validator.removeField(namePrefix + "[primary][]");
+        }
+
+        $(form).repeater({
+            initEmpty: false,
+
+            show: function () {
+                $(this).slideDown();
+
+                const index = $(this).closest("[data-repeater-item]").index();
+
+                addFields(index);
+            },
+
+            hide: function (deleteElement) {
+                $(this).slideUp(deleteElement);
+                const index = $(this).closest("[data-repeater-item]").index();
+                removeFields(index);
+            }
+        });
+
+        // Initial fields
+        addFields(0);
+
+        // Submit button handler
+        const submitButton = document.getElementById("kt_docs_repeater_button");
+        submitButton.addEventListener("click", function (e) {
+            // Prevent default button action
+            e.preventDefault();
+
+            // Validate form before submit
+            if (validator) {
+                validator.validate().then(function (status) {
+                    if (status == "Valid") {
+                        // Show loading indication
+                        submitButton.setAttribute("data-kt-indicator", "on");
+
+                        // Disable button to avoid multiple click
+                        submitButton.disabled = true;
+
+                        // Simulate form submission. For more info check the plugin's official documentation:https://sweetalert2.github.io/
+                        setTimeout(function () {
+                            // Remove loading indication
+                            submitButton.removeAttribute("data-kt-indicator");
+
+                            // Enable button
+                            submitButton.disabled = false;
+
+                            // Show popup confirmation
+                            Swal.fire({
+                                text: "Form has been successfully submitted!",
+                                icon: "success",
+                                buttonsStyling: false,
+                                confirmButtonText: "Ok, got it!",
+                                customClass: {
+                                    confirmButton: "btn btn-primary"
+                                }
+                            });
+
+                            //form.submit(); // Submit form
+                        }, 2000);
+                    }
+                });
+            }
+        });
+    }
+});
